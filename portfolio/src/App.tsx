44d7406888d0d5fb60f0e6d61336199617375dce
Vinícius Borges <viniborg25@gmail.com>
2023-09-05 21:39:01 -0300

    Initial commit

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,13 @@
+import "./styles/main.css"
+
+function App() {
+  return (
+    <>
+      <div className="bg-[#0a0b4e] w-screen h-screen">
+        
+      </div>
+    </>
+  )
+}
+
+export default App
